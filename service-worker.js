66b0e8e2550f177db0035c92d9c2e219e58fc2bf
SiddharthaMishra <sidm1999@gmail.com>
2018-05-30 20:10:50 +0530

    added caching to service worker

---
+++ service-worker.js
@@ -0,0 +1,109 @@
+/* Cache versions which are supposed to be used */
+var expectedCaches = ['v1'];
+
+/* Cache all data from our site for offline viewing 
+ * Change version number when cache is updated */
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(
+        [
+          'css/animate.css',
+          'css/animations.css',
+          'css/bootstrap.css',
+          'css/color.css',
+          'css/custom-style.css',
+          'css/flexslider.css',
+          'css/jpreloader.css',
+          'css/mb.YTPlayer.css',
+          'css/owl.carousel.css',
+          'css/owl.theme.css',
+          'css/owl.transitions.css',
+          'css/plugin.css',
+          'css/prettyPhoto.css',
+          'css/rev-settings.css',
+          'css/style.css',
+          'js/bootstrap.min.js',
+          'js/classie.js',
+          'js/countUp.min.js',
+          'js/custom.js',
+          'js/designesia.js',
+          'js/device.min.js',
+          'js/easing.js',
+          'js/ender.js',
+          'js/html5shiv.js',
+          'js/jpreLoader.js',
+          'js/jpreLoader.min.js',
+          'js/jquery.backstretch.min.js',
+          'js/jquery.countTo.js',
+          'js/jquery.flexslider-min.js',
+          'js/jquery.isotope.min.js',
+          'js/jquery.mb.YTPlayer.js',
+          'js/jquery.min.js',
+          'js/jquery.prettyPhoto.js',
+          'js/jquery.scrollto.js',
+          'js/jquery.stellar.min.js',
+          'js/jquery.tubular.1.0b.js',
+          'js/jquery.tubular.1.0.js',
+          'js/jquery.ui.map.js',
+          'js/jquery.ui.totop.js',
+          'js/map.js',
+          'js/maps.api.js',
+          'js/owl.carousel.js',
+          'js/validation.js',
+          'js/video.resize.js',
+          'images/avatar.jpg',
+          'images/background',
+          'images/blank.png',
+          'images/blog',
+          'images/close-2.png',
+          'images/close.png',
+          'images/dot-black.png',
+          'images/dot.png',
+          'images/favicon.jpg',
+          'images/icon-slide-min.png',
+          'images/icon-slide-plus.png',
+          'images/logo',
+          'images/logo-1.jpg',
+          'images/logo-2.jpg',
+          'images/logo-2.png',
+          'images/logo-3.jpg',
+          'images/logo.jpg',
+          'images/logo.png',
+          'images/page-loader.gif',
+          'images/pat-1.png',
+          'images/prettyPhoto',
+          'images/testi-default.jpg',
+          'images/trial1.png',
+          'images/ui.totop.png',
+        ]
+      );
+    })
+  );
+});
+
+
+
+/* Fall back onto the cache if fetch from the network fails */
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
+
+
+
+/* Delete any old caches in case of updates */
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(function(keys) { 
+      return Promise.all(keys.map(function(key) {
+        if (!expectedCaches.includes(key)) {
+          return caches.delete(key);
+        } 
+      }));
+    })
+  );
+});
